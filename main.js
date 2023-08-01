23071cae7bcf963f4edcc2a5fd60b88751f8a9c4
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-08-01 23:36:57 +0100

    Registration page - getting started

--- main.js
+++ main.js
@@ -120,18 +120,3 @@ form2.addEventListener('submit', (e) => {
         window.location.href = 'home-page.html';
     }
 })
-
-
-
-// //HOME PAGE
-
-// const hamburgerMenu = document.querySelector('.hamburger-menu');
-// const line1 = document.querySelector('.line-1');
-// const line2 = document.querySelector('.line-2');
-// const line3 = document.querySelector('.line-3');
-
-// hamburgerMenu.addEventListener('click', () => {
-//     line1.classList.toggle('close');
-//     line2.classList.toggle('gone');
-//     line3.classList.toggle('close-2');
-// });
